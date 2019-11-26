73f30ce8e9989750478992f5fd6684377a7645e5
shokts@wincs.cs.bgu.ac.il <shokts@post.bgu.ac.il>
2019-11-26 17:47:15 +0200

    s

---
+++ cmake-build-debug/CMakeFiles/Assingment1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/src/Action.cpp" "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/cmake-build-debug/CMakeFiles/Assingment1.dir/src/Action.cpp.o"
+  "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/src/Main.cpp" "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/cmake-build-debug/CMakeFiles/Assingment1.dir/src/Main.cpp.o"
+  "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/src/Session.cpp" "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/cmake-build-debug/CMakeFiles/Assingment1.dir/src/Session.cpp.o"
+  "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/src/User.cpp" "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/cmake-build-debug/CMakeFiles/Assingment1.dir/src/User.cpp.o"
+  "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/src/Watchable.cpp" "/users/studs/bsc/2020/shokts/CLionProjects/Assingment1/cmake-build-debug/CMakeFiles/Assingment1.dir/src/Watchable.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Assingment1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "Assingment1"
+  "Assingment1.pdb"
+  "CMakeFiles/Assingment1.dir/src/Action.cpp.o"
+  "CMakeFiles/Assingment1.dir/src/Main.cpp.o"
+  "CMakeFiles/Assingment1.dir/src/Session.cpp.o"
+  "CMakeFiles/Assingment1.dir/src/User.cpp.o"
+  "CMakeFiles/Assingment1.dir/src/Watchable.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assingment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
